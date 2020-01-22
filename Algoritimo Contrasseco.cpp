0f74477544dd78fbb81349ee00a68d167868c561
leopesi <leopesi@yahoo.com.br>
2020-01-22 17:18:05 -0300

    Melhoria no algorítimo

--- Algoritimo Contrasseco.cpp
+++ Algoritimo Contrasseco.cpp
@@ -11,86 +11,85 @@ int luz3 = 11;
 int luz4 = 12;
 int luz5 = 4;
 
-
-
 void setup(){
+
   pinMode(Sensor,INPUT);
   pinMode(Motor, OUTPUT);
   digitalWrite(Motor,HIGH);
   delay (10000);}
 
 void loop(){
-  
-  
+
   switch (digitalRead(Sensor)){
-    
-    for(Estado = 1; Estado < 6; Estado++) {
-    
-    case 0: {
+
+  //Fluxo dágua normal
+    case 0:
       digitalWrite(Motor,HIGH);
       digitalWrite(Nosafer,LOW);
       digitalWrite(Safer,HIGH);
       digitalWrite(Setup,LOW);
-      
+
       digitalWrite(luz1,LOW);
       digitalWrite(luz2,LOW);
       digitalWrite(luz3,LOW);
       digitalWrite(luz4,LOW);
       digitalWrite(luz5,LOW);
-      Estado = 1;}
-    
-    break;
-    
-    case 1: {
-      digitalWrite(Motor,LOW);
-      digitalWrite(Nosafer,HIGH);
-      digitalWrite(Safer,LOW);
-      digitalWrite(Setup,LOW);}
-    
-     
-    if (Estado == 1){ // 1� TENTATIVA
-      digitalWrite(luz1,HIGH);
-      delay (15000);
-      digitalWrite(Motor,HIGH);
-      delay (5000);}
-       
-    else if (Estado == 2){ // 2� TENTATIVA
-      digitalWrite(luz2,HIGH);
-      delay (3000);
-      digitalWrite(Motor,HIGH);
-      delay (5000);}
-      
-    else if (Estado == 3){ // 3� TENTATIVA
-      digitalWrite(luz3,HIGH);
-      delay (10000);
-      digitalWrite(Motor,HIGH);
-      delay (5000);}
-      
-    else if (Estado == 4){ // 4� TENTATIVA
-      digitalWrite(luz4,HIGH);
-      delay (10000);
-      digitalWrite(Motor,HIGH);
-      delay (5000);}
-   
-    
-    else if (Estado == 5){ // 5� TENTATIVA
-      digitalWrite(luz5,HIGH);
-      delay (5000);
-      digitalWrite(Motor,HIGH);
-      delay (12000);}
-      
-    else {
-      digitalWrite(Motor,LOW);
-      digitalWrite(Nosafer,LOW);
-      digitalWrite(Setup,LOW);
-      delay (700);
-      digitalWrite(Setup,HIGH);
-      delay (700);
-      }
-      
-    }
-  }
-  
-}
+      break;
 
-    
+  //Fluxo dágua interrompido
+    case 1:
+
+
+        for(Estado = 1; Estado >= 1; Estado++) {
+
+            digitalWrite(Motor,LOW);
+            digitalWrite(Nosafer,HIGH);
+            digitalWrite(Safer,LOW);
+            digitalWrite(Setup,LOW);
+
+          // TENTATIVA 1
+            if ( (Estado == 1) && (digitalRead(Sensor) == 1) ) { 
+                digitalWrite(luz1,HIGH);
+                delay (10000);
+                digitalWrite(Motor,HIGH);
+                delay (5000); }
+          
+          // TENTATIVA 2
+            else if ( (Estado == 2) && (digitalRead(Sensor) == 1) ) { 
+                digitalWrite(luz2,HIGH);
+                delay (3000);
+                digitalWrite(Motor,HIGH);
+                delay (5000); }
+          
+          // TENTATIVA 3
+            else if ( (Estado == 3) && (digitalRead(Sensor) == 1) ) { 
+                digitalWrite(luz3,HIGH);
+                delay (10000);
+                digitalWrite(Motor,HIGH);
+                delay (5000); }
+          
+          // TENTATIVA 4
+            else if ( (Estado == 4) && (digitalRead(Sensor) == 1) ) { 
+                digitalWrite(luz4,HIGH);
+                delay (10000);
+                digitalWrite(Motor,HIGH);
+                delay (5000); }
+          
+			    // TENTATIVA 5
+            else if ( (Estado == 5) && (digitalRead(Sensor) == 1) ) { 
+                digitalWrite(luz5,HIGH);
+                delay (5000);
+                digitalWrite(Motor,HIGH);
+                delay (12000); }
+          
+			// Luz Setup 
+            else if ( (Estado > 5) && (digitalRead(Sensor) == 1) ){
+                digitalWrite(Motor,LOW);
+                digitalWrite(Nosafer,LOW);
+                digitalWrite(Setup,LOW);
+                delay (900);
+                digitalWrite(Setup,HIGH);
+                delay (900);}
+          }     
+  }
+}
